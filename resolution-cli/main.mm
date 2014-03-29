ebf4f7066a9b10968458911964b8e26f78b6eeba
Anthony Dervish <antmd@mac.com>
2014-03-29 23:26:31 +0000

    Allow missing display index => main display; fix resolution spec. parsing

--- resolution-cli/main.mm
+++ resolution-cli/main.mm
@@ -3,6 +3,7 @@
 //  resolution-cli
 //
 //  Created by Anthony Dervish on 01/02/2014.
+//  Based on 'ResolutionMenu' by Robbert Klarenbeek.
 //  Copyright (c) 2014 Robbert Klarenbeek. All rights reserved.
 //
 
@@ -19,6 +20,37 @@
 #include <algorithm>
 #include <libgen.h> // For basename
 using namespace std;
+
+
+/*
+ *
+ *
+ *================================================================================================*/
+#pragma mark - Private CoreGraphics API
+/*==================================================================================================
+ */
+
+// CoreGraphics DisplayMode struct used in private APIs
+typedef struct {
+    uint32_t modeNumber;
+    uint32_t flags;
+    uint32_t width;
+    uint32_t height;
+    uint32_t depth;
+    uint8_t unknown[170];
+    uint16_t freq;
+    uint8_t more_unknown[16];
+    float density;
+} CGSDisplayMode;
+
+extern "C" {
+// CoreGraphics private APIs with support for scaled (retina) display modes
+void CGSGetCurrentDisplayMode(CGDirectDisplayID display, int *modeNum);
+void CGSConfigureDisplayMode(CGDisplayConfigRef config, CGDirectDisplayID display, int modeNum);
+void CGSGetNumberOfDisplayModes(CGDirectDisplayID display, int *nModes);
+void CGSGetDisplayModeDescriptionOfLength(CGDirectDisplayID display, int idx, CGSDisplayMode *mode, int length);
+CGDirectDisplayID CGSMainDisplayID(void);
+}
 
 
 struct DisplayMode
@@ -39,6 +71,15 @@ struct DisplayMode
     ,active(active)
     {;}
     
+    DisplayMode(CGSDisplayMode mode) :
+                    modeNumber(mode.modeNumber),
+                    width(mode.width),
+                    height(mode.height),
+                    depth(mode.depth),
+                    hidpi(mode.density>1.5),
+                    active(false)
+    {;}
+
     bool operator<(const DisplayMode& rhs) const {
         if (width < rhs.width) { return true; }
         else if (width == rhs.width) {
@@ -79,44 +120,79 @@ using DisplayID = CGDirectDisplayID;
 
 struct DisplayInfo
 {
+    using DisplayInfoVec = vector<DisplayInfo>;
+    static unique_ptr<DisplayInfoVec> sDisplayInfo;
+    
     DisplayID displayID;
     string name;
     DisplayModes displayModes;
     DisplayInfo(DisplayID displayID, string name, DisplayModes displayModes) :
     displayID(displayID), name(move(name)), displayModes(move(displayModes)) {;}
+    
+    static const DisplayInfoVec& allDisplayInfo(void) {
+        return _getDisplayModes();
+    }
+    
+    static const DisplayInfo& mainDisplayInfo() {
+        return _getDisplayModes()[0];
+    }
+    
+private:
+    static const DisplayInfoVec& _getDisplayModes() {
+        // One-shot mode -- we do not have to take account of changed display info
+        
+        if (!sDisplayInfo) {
+            sDisplayInfo.reset(new DisplayInfoVec());
+            uint32_t numberOfDisplays;
+            CGDirectDisplayID displays[16];
+            CGDirectDisplayID mainDisplay = CGSMainDisplayID();
+            CGGetOnlineDisplayList(sizeof(displays) / sizeof(displays[0]), displays, &numberOfDisplays);
+            for (int i = 0; i < numberOfDisplays; i++) {
+                CGDirectDisplayID display = displays[i];
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+                NSDictionary *deviceInfo = (__bridge NSDictionary *)IODisplayCreateInfoDictionary(CGDisplayIOServicePort(display), kIODisplayOnlyPreferredName);
+    #pragma clang diagnostic pop
+                NSDictionary *localizedNames = [deviceInfo objectForKey:[NSString stringWithUTF8String:kDisplayProductName]];
+                NSString* displayName = localizedNames[[localizedNames allKeys][0]];
+                
+                // Get the current display mode, so we can put a checkmark (NSOnState) next to it
+                int currentDisplayModeNumber;
+                CGSGetCurrentDisplayMode(display, &currentDisplayModeNumber);
+                
+                // Loop through all display modes, but only use 1 for each unique title
+                int numberOfDisplayModes;
+                CGSGetNumberOfDisplayModes(display, &numberOfDisplayModes);
+                DisplayModes displayModes;
+                
+                for (int i = 0; i < numberOfDisplayModes; i++) {
+                    CGSDisplayMode mode;
+                    CGSGetDisplayModeDescriptionOfLength(display, i, &mode, sizeof(mode));
+                    
+                    displayModes.emplace(
+                        mode.modeNumber,
+                        mode.width,
+                        mode.height,
+                        mode.depth,
+                        (mode.density>1.5),
+                        (mode.modeNumber == currentDisplayModeNumber)
+                    );
+                    
+                }
+                if (display == mainDisplay) {
+                    // Ensure main display is at index 0
+                    sDisplayInfo->insert(sDisplayInfo->begin(), DisplayInfo(display,string(displayName.UTF8String),displayModes));
+                }
+                else {
+                    sDisplayInfo->emplace_back(display, string(displayName.UTF8String), displayModes);
+                }
+            }
+        }
+        return *sDisplayInfo;
+    }
 };
 
-using DisplayToDisplayInfo = vector<DisplayInfo>;
-
-
-/*
- *
- *
- *================================================================================================*/
-#pragma mark - Private CoreGraphics API
-/*==================================================================================================
- */
-
-// CoreGraphics DisplayMode struct used in private APIs
-typedef struct {
-    uint32_t modeNumber;
-    uint32_t flags;
-    uint32_t width;
-    uint32_t height;
-    uint32_t depth;
-    uint8_t unknown[170];
-    uint16_t freq;
-    uint8_t more_unknown[16];
-    float density;
-} CGSDisplayMode;
-
-extern "C" {
-// CoreGraphics private APIs with support for scaled (retina) display modes
-void CGSGetCurrentDisplayMode(CGDirectDisplayID display, int *modeNum);
-void CGSConfigureDisplayMode(CGDisplayConfigRef config, CGDirectDisplayID display, int modeNum);
-void CGSGetNumberOfDisplayModes(CGDirectDisplayID display, int *nModes);
-void CGSGetDisplayModeDescriptionOfLength(CGDirectDisplayID display, int idx, CGSDisplayMode *mode, int length);
-}
+/*static*/ unique_ptr<DisplayInfo::DisplayInfoVec> DisplayInfo::sDisplayInfo;
 
 
 /*
@@ -134,8 +210,13 @@ void usage(const char* binary) {
     << " Usage: " << binaryName << " <command> [<argument> <argument>]" << endl
     << R"(
     Commands:
-        list - list the available resolutions
-        set <display-index> <resolution> - set the resolution
+    
+    list
+        list the available resolutions
+
+    set [<display-index>] <resolution>
+        set the resolution. If no display-index is specified, set the main display resolution
+    
     
     <resolution> can be specified in several ways, and an underscore can be used
     anywhere a number might be used meaning 'match anything' in a search from highest-
@@ -152,54 +233,8 @@ void usage(const char* binary) {
     )" << endl;
 }
 
-DisplayToDisplayInfo getDisplayModes() {
-    // One-shot mode -- we do not have to take account of changed display info
-    static unique_ptr<DisplayToDisplayInfo> sDisplayInfo;
-    
-    if (!sDisplayInfo) {
-        sDisplayInfo.reset(new DisplayToDisplayInfo());
-        uint32_t numberOfDisplays;
-        CGDirectDisplayID displays[16];
-        CGGetOnlineDisplayList(sizeof(displays) / sizeof(displays[0]), displays, &numberOfDisplays);
-        for (int i = 0; i < numberOfDisplays; i++) {
-            CGDirectDisplayID display = displays[i];
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-            NSDictionary *deviceInfo = (__bridge NSDictionary *)IODisplayCreateInfoDictionary(CGDisplayIOServicePort(display), kIODisplayOnlyPreferredName);
-#pragma clang diagnostic pop
-            NSDictionary *localizedNames = [deviceInfo objectForKey:[NSString stringWithUTF8String:kDisplayProductName]];
-            NSString* displayName = localizedNames[[localizedNames allKeys][0]];
-            
-            // Get the current display mode, so we can put a checkmark (NSOnState) next to it
-            int currentDisplayModeNumber;
-            CGSGetCurrentDisplayMode(display, &currentDisplayModeNumber);
-            
-            // Loop through all display modes, but only use 1 for each unique title
-            int numberOfDisplayModes;
-            CGSGetNumberOfDisplayModes(display, &numberOfDisplayModes);
-            DisplayModes displayModes;
-            
-            for (int i = 0; i < numberOfDisplayModes; i++) {
-                CGSDisplayMode mode;
-                CGSGetDisplayModeDescriptionOfLength(display, i, &mode, sizeof(mode));
-                
-                displayModes.emplace(
-                    mode.modeNumber,
-                    mode.width,
-                    mode.height,
-                    mode.depth,
-                    (mode.density>1.5),
-                    (mode.modeNumber == currentDisplayModeNumber)
-                );
-                
-            }
-            sDisplayInfo->emplace_back(display, string(displayName.UTF8String), displayModes);
-        }
-    }
-    return *sDisplayInfo;
-}
 void listDisplayModes() {
-    DisplayToDisplayInfo displayInfo(getDisplayModes());
+    const DisplayInfo::DisplayInfoVec &displayInfo(DisplayInfo::allDisplayInfo());
     for (size_t idx = 0; idx < displayInfo.size(); ++idx ) {
         cout << idx << ": " << displayInfo[idx].name << endl;
         for ( const DisplayMode& info : displayInfo[idx].displayModes ) {
@@ -222,22 +257,32 @@ void changeDisplayMode(CGDirectDisplayID display, int modeNumber)
 DisplayMode displayModeFromString(const string& displayStr) {
     
     DisplayMode displayMode;
-    regex sizeRe(R"((_|[0-9]+)x(_|[0-9]+))");
+    regex sizeRe("^(_|[0-9]+)x(_|[0-9]+)");
+    regex sizeRe2("^(_|[0-9]+)");
     smatch sizeMatch;
+    string restOfString(displayStr);
+    bool matched(false);
     try {
-        string restOfString(displayStr);
         if (regex_search(displayStr, sizeMatch, sizeRe)) {
             displayMode.width = sizeMatch[1]=="_" ? -1 : stoi(sizeMatch[1]);
             displayMode.height = sizeMatch[2]=="_" ? -1 : stoi(sizeMatch[2]);
             
             restOfString = sizeMatch.suffix().str();
+            matched = true;
+        }
+        else if (regex_search(displayStr,sizeMatch,sizeRe2)) {
+            displayMode.width = sizeMatch[1]=="_" ? -1 : stoi(sizeMatch[1]);
+            displayMode.height = -1 ;
             
-            regex depthRe(R"(@(_|[0-9]+))");
-            smatch depthMatch;
-            if (regex_search(restOfString,depthMatch,depthRe)) {
-                displayMode.depth = log2(stoi(depthMatch[1]))-1;
-                restOfString = depthMatch.suffix().str();
-            }
+            restOfString = sizeMatch.suffix().str();
+            matched = true;
+        }
+        
+        regex depthRe("@(_|[0-9]+)");
+        smatch depthMatch;
+        if (regex_search(restOfString,depthMatch,depthRe)) {
+            displayMode.depth = log2(stoi(depthMatch[1]))-1;
+            restOfString = depthMatch.suffix().str();
         }
         
         regex hidpiRe(R"(^ *h(i(d(p(i)?)?)?)?)",regex_constants::icase);
@@ -258,7 +303,7 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        DisplayToDisplayInfo displayModes(getDisplayModes());
+        const DisplayInfo::DisplayInfoVec &displayModes(DisplayInfo::allDisplayInfo());
         
         if (argc < 2) {
             usage(argv[0]);
@@ -266,13 +311,18 @@ int main(int argc, const char * argv[])
         }
         
         if (string("set") == argv[1]) {
-            if (argc<4) {
-                cerr << "Must supply a display and resolution to 'set' command" << endl;
+            if (argc<3) {
+                cerr << "Must supply a resolution, or display and resolution to 'set' command" << endl;
                 usage(argv[0]);
                 exit(1);
             }
-            size_t selectedDisplayIdx(stod(argv[2]));
-            DisplayMode selectedMode(displayModeFromString(argv[3]));
+            
+            size_t selectedDisplayIdx(argc==3?0:stod(argv[2]));
+            if (selectedDisplayIdx>=displayModes.size()) {
+                cerr << "Illegal display idx '" << selectedDisplayIdx << "'. Must be in range 0 to " << displayModes.size()-1 << endl;
+                exit(1);
+            }
+            DisplayMode selectedMode(displayModeFromString(argc==3?argv[2]:argv[3]));
             
             DisplayInfo infoForSelectedDisplay(displayModes[selectedDisplayIdx]);
             DisplayID selectedDisplayID(infoForSelectedDisplay.displayID);
